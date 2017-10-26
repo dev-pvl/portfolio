60f607298682009238ec1d515cff107b64e2ef78
dev_pavel <pavel.syrotyna@gmail.com>
2017-10-26 12:40:04 +0300

    add home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import '../scss/Home.scss';
 
 const Home = () => (
-  <div>
-    <h1>Welcome to the Tornadoes Website!</h1>
+  <div className="greeting">
+    <h1 className="greeting__head">Paul</h1>
+    <h2 className="greeting__subhead">Front-end<br></br>developer</h2>
   </div>
 )
 
