fe5fa2983fdb18e63ddd97b465fd2ceb0748abeb
dev_pavel <pavel.syrotyna@gmail.com>
2017-11-30 11:14:25 +0200

    about styles changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import '../scss/About.scss'
+import face from '../img/normal.jpg';
+
+;(function(window) {
+  function getMousePos(e) {
+    var posx = 0;
+    var posy = 0;
+    if (!e) var e = window.event;
+    if (e.pageX || e.pageY) 	{
+      posx = e.pageX;
+      posy = e.pageY;
+    }
+    else if (e.clientX || e.clientY) 	{
+      posx = e.clientX + document.body.scrollLeft
+        + document.documentElement.scrollLeft;
+      posy = e.clientY + document.body.scrollTop
+        + document.documentElement.scrollTop;
+    }
+    return {
+      x : posx,
+      y : posy
+    }
+  }
+})(window);
+
 
 class About extends React.Component {
+
   render() {
     return (
       <div>
         <h1>About!</h1>
+        <img className="face" src={face} alt="face"></img>
       </div>
     )
   }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import {AreaChart, Area} from 'recharts/es6'
+
+const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+      {name: 'Page H', uv: 5000, pv: 4500, amt: 2700},
+];
 
 class Work extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Work!</h1>
-      </div>
+      <AreaChart width={window.innerWidth} height={window.innerHeight} data={data}
+            margin={{top: 50, right: 0, left: 60, bottom: 5}}>
+        <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
+      </AreaChart>
     )
   }
 }
